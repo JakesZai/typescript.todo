5d8db94fc067d7f445c45ada126c7aa985eb9b0e
anirudhit <anirudhit@outlook.com>
2017-11-06 21:32:38 +0200

    Step-3 "Typescript Let and const"

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var container = document.getElementById('container');
-var todo = {
-    id: 123,
-    name: 'Pick up drycleaning',
-    completed: true
-};
-container.innerHTML = "\n\t<div todo=\"" + todo.id + "\" class=\"todo-container\">\n\t\t<div class=\"" + (todo.completed ? "" : "hidden") + " primary text-primary\">\n\t\t\t<span>" + todo.name + "</span>\n\t\t</div>\n\t</div>\n";
+for (var x = 0; x <= 5; x++) {
+    var counter = x;
+    counter = 1;
+}
+console.log(counter);
--- app.ts
+++ app.ts
@@ -1,15 +1,7 @@
 var container = document.getElementById('container');
 
-var todo = {
-	id: 123,
-	name: 'Pick up drycleaning',
-	completed: true
+for (var x = 0; x <= 5; x++){
+	const counter = x;
+	counter = 1;
 }
-
-container.innerHTML = `
-	<div todo="${todo.id}" class="todo-container">
-		<div class="${todo.completed ? "" : "hidden"} primary text-primary">
-			<span>${todo.name}</span>
-		</div>
-	</div>
-`;
+console.log(counter);
